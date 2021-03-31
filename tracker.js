9a601c3019a82b14444f631b254b0235ce9cafd2
Prithviraj Dey Purkayastha <prithviraj.deyp@gmail.com>
2021-03-31 22:17:30 +0530

    Update tracker.js

--- tracker.js
+++ tracker.js
@@ -3,6 +3,13 @@ var firstTimeDisplay = function(){
       console.log('Tracker Says: Language :'+navigator.language);
       console.log('Tracker Says: Account ID : humainetest');
       console.log('Tracker Says: Session ID : session1');
+      console.log('in get awareness');
+      const queryString = window.location.search;
+      const urlParams = new URLSearchParams(queryString);
+      if(urlParams.get('campaign'))
+      {
+        console.log('Tracker Says: Campaign ID : '+urlParams.get('campaign');)
+       }
     };
     var pageNavigate = function(pageName){
       console.log('Tracker Says : On '+pageName+' page');
